b8ab4c4200d6833438d2cd6a4efb870a4ab7188c
mori-dev <mori.dev.asdf@gmail.com>
2015-03-01 19:41:06 +0900

    json で返すようにした

--- src/main/scala/MyStatusApiService.scala
+++ src/main/scala/MyStatusApiService.scala
@@ -5,20 +5,34 @@ import akka.event.Logging
 import spray.routing._
 import spray.http._
 import MediaTypes._
+import spray.json._
+import spray.httpx.marshalling._
+import spray.httpx.SprayJsonSupport._
 
 class MyStatusApiServiceActor extends Actor with MyStatusApiService {
   def actorRefFactory = context
   def receive = runRoute(myStatusApiRoute)
 }
 
+case class BizData(status: String, term: String, message: String)
+
+object MyJsonProtocol extends DefaultJsonProtocol {
+  implicit val bizDataFormat = jsonFormat3(BizData)
+}
+
 trait MyStatusApiService extends HttpService {
+
+  import MyJsonProtocol._
+
   val myStatusApiRoute =
     pathPrefix("api") {
       pathPrefix("v1") {
         path("mori_dev" / "status") {
           get {
-            complete {
-              <h1>I am super busy. Thank you.</h1>
+            respondWithMediaType(`application/json`) {
+              complete {
+                marshal(BizData("I am super busy.", "4月末まで", "Brought to You By Scala/spray"))
+              }
             }
           }
         }
